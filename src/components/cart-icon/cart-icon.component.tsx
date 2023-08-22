8e8c19428b603b7e0c4bae3eaf52d7c43d4da65d
lingeorge88 <lingeorge04@gmail.com>
2023-08-22 16:02:03 -0700

    typed svg logos

--- src/components/cart-icon/cart-icon.component.tsx
+++ src/components/cart-icon/cart-icon.component.tsx
@@ -1,12 +1,9 @@
-import {React}from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-// import { CartContext } from '../../contexts/cart.context';
 import { selectCartCount, selectIsCartOpen } from '../../store/cart/cart.selector';
 import { setIsCartOpen } from '../../store/cart/cart.action';
 import {ShoppingIcon, CartIconContainer, ItemCount } from './cart-icon.styles.jsx';
 
 const CartIcon = () => {
-    // const {isCartOpen, setIsCartOpen, cartCount} = useContext(CartContext);
     const dispatch = useDispatch();
      const cartCount = useSelector(selectCartCount);
     const isCartOpen = useSelector(selectIsCartOpen);
