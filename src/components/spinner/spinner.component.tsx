1d7b51378aca386f49ecd838af16af9ef85330eb
lingeorge88 <lingeorge04@gmail.com>
2023-09-01 19:02:56 -0700

    added test to mock react-router-dom to test category rendering

--- src/components/spinner/spinner.component.tsx
+++ src/components/spinner/spinner.component.tsx
@@ -2,7 +2,7 @@ import { SpinnerOverlay, SpinnerContainer } from "./spinner.styles";
 
 export const Spinner = () => {
     return (
-        <SpinnerOverlay>
+        <SpinnerOverlay data-testid='spinner'>
             <SpinnerContainer />
         </SpinnerOverlay>
     )
