6a397e89c0d1038f26fe502fafbce4e40772d8c4
lingeorge88 <lingeorge04@gmail.com>
2023-10-04 21:03:19 -0700

    added test for cart dropdown component

--- src/components/cart-dropdown/__tests__/cartDropdown.test.js
+++ src/components/cart-dropdown/__tests__/cartDropdown.test.js
@@ -1,9 +1,40 @@
-import { render, fireEvent } from '@testing-library/react';
+import { screen, render, fireEvent } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
 import { MemoryRouter } from 'react-router-dom';
+import {createStore} from 'redux';
 import CartDropdown from '../cart-dropdown.component';
-
+const mockReducer = (state = { cart: { cartItems: [] } }, action) => state;
 describe("Tests that the cart dropdown component renders and functions correctly", ()=> {
-    test('')
+    test('renders cart items when cart is not empty', () => {
+        // Mock the Redux store with initial state
+        const mockStore = createStore(mockReducer, {
+          cart: { cartItems: [{ id: '1', name: 'testItem' }] } // Mocked cart items
+        });
+    
+        const { getByText } = render(
+          <Provider store={mockStore}>
+            <MemoryRouter>
+              <CartDropdown />
+            </MemoryRouter>
+          </Provider>
+        );
+    
+        expect(screen.getByText('testItem')).toBeInTheDocument();
+      });
+
+    test('renders the empty message when the cart is empty', ()=>{
+        const mockStore = createStore(mockReducer, {
+            cart: { cartItems: [] } // Empty cart
+          });
+
+          const { getByText } = render(
+            <Provider store={mockStore}>
+                <MemoryRouter>
+                    <CartDropdown/>
+                </MemoryRouter>
+            </Provider>
+          );
+
+          expect(screen.getByText('Your cart is empty')).toBeInTheDocument();
+    })
 })
